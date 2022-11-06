7536af9d8283e2e8f5727fb5d023a0cba5d8176f
Filip Cheliński <filipchel20@gmail.com>
2022-11-06 16:09:36 -0200

    Add subreducers and remove cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/store';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = ({ id, title, isFavorite }) => {
 
@@ -10,12 +10,21 @@ const dispatch = useDispatch();
 const toggle = () => {
     dispatch(toggleCardFavorite(id, isFavorite));
   }
+
+const remove = () => {
+    dispatch(removeCard(id));
+  }
     return (
         <li className={styles.card}>{title}
+            <div>
 		  <button onClick={toggle} className={clsx(styles.star_button, isFavorite && styles.active)}>
 				<span className='fa fa-star-o' />
 			</button>
-		</li>
+            <button onClick={remove} className={clsx(styles.remove_button)} >
+        <span className='fa fa-trash' />
+         </button>
+           </div>
+	    </li>
     );
 };
 
